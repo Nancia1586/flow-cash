73a082a4ad16816f512c4dcb71441e061504eaae
Nancia Rajerison <rajerisonnancia@gmail.com>
2025-03-10 22:32:39 +0300

    Update page.tsx

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -3,10 +3,10 @@ import { RegisterForm } from "./components/register-form";
 
 export default function RegisterPage() {
   return (
-    <div className="bg-custom-light-green flex items-center justify-center w-full min-w-screen min-h-screen">
+    <div className="bg-custom-light-green flex items-center justify-center w-full min-w-screen min-h-screen lg:p-10">
       <div className="bg-white shadow-xl mx-auto grid rounded-sm w-full min-h-screen lg:w-auto lg:min-h-0">
         <div className="p-10 lg:flex flex-col justify-center items-center w-full min-h-screen lg:min-h-0">
-          <div className="lg:hidden flex justify-center mb-4">
+          <div className="flex justify-center mb-4">
             <Image
               src="/flow-cash-logo.png"
               width={120}
