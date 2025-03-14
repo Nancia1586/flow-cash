33e98875b74e6230d5c2fbcea7b8b7b7a4275841
Nancia Rajerison <rajerisonnancia@gmail.com>
2025-03-14 12:41:18 +0300

    Re-organize codes

--- app/home/dashboard/page.tsx
+++ app/home/dashboard/page.tsx
@@ -1,37 +1,9 @@
-import { AmountCard } from "./components/amount-card";
-import {
-  FcBearish,
-  FcBullish,
-  FcMoneyTransfer,
-  FcSalesPerformance,
-} from "react-icons/fc";
+import { CardSection } from "./components/sections/card-section";
 
 export default function Dashboard() {
   return (
-    <div className="w-full space-y-4 p-4">
-      <div className="w-full rounded-sm border p-4"></div>
-      <div className="w-full grid grid-cols-2 lg:grid-cols-4 gap-4">
-        <AmountCard
-          icon={<FcMoneyTransfer />}
-          title="Mon solde"
-          amount={12250000}
-        />
-        <AmountCard
-          icon={<FcSalesPerformance />}
-          title="Mon epargne"
-          amount={2389000}
-        />
-        <AmountCard
-          icon={<FcBullish />}
-          title="Mes revenus"
-          amount={23450000}
-        />
-        <AmountCard
-          icon={<FcBearish />}
-          title="Mes dépenses"
-          amount={11267400}
-        />
-      </div>
+    <div className="w-full p-4">
+      <CardSection />
     </div>
   );
 }
