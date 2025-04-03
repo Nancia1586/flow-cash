cd95a14fab8e6225943c687913f9ce1b7615bee3
Nancia Rajerison <rajerisonnancia@gmail.com>
2025-04-03 18:45:57 +0300

    Update app layout

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -1,22 +1,15 @@
 import { AppSidebar } from "@/components/app-sidebar";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { SiteHeader } from "@/components/site-header";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
-      <AppSidebar />
-      <main className="w-screen">
-        <div className="flex items-center justify-between w-min-screen w-full h-18 border-b px-3">
-          <SidebarTrigger />
-          <div className="flex items-center gap-1">
-            <div className="bg-custom-blue-gray text-white rounded-full px-3 py-1 text-lg">
-              R
-            </div>
-            <span className="text-primary">Rakoto</span>
-          </div>
-        </div>
+      <AppSidebar variant="inset" />
+      <SidebarInset>
+        <SiteHeader />
         {children}
-      </main>
+      </SidebarInset>
     </SidebarProvider>
   );
 }
