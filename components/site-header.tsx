c974b50471a89179fc2cf9408e9a3b6f77b7d086
Nancia Rajerison <rajerisonnancia@gmail.com>
2025-04-08 22:04:12 +0300

    Dynamization of the page title

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,7 +1,25 @@
+"use client";
+
 import { Separator } from "@/components/ui/separator";
 import { SidebarTrigger } from "@/components/ui/sidebar";
+import { usePathname } from "next/navigation";
 
 export function SiteHeader() {
+  const pathname = usePathname();
+
+  const getPageTitle = (path: string) => {
+    switch (path) {
+      case "/home/dashboard":
+        return "Tableau de bord";
+      case "/home/transactions":
+        return "Mes transactions";
+      case "/home/savings":
+        return "Mon épargne";
+      default:
+        return "Tableau de bord";
+    }
+  };
+
   return (
     <header className="group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 flex h-12 shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear">
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
@@ -10,7 +28,7 @@ export function SiteHeader() {
           orientation="vertical"
           className="mx-2 data-[orientation=vertical]:h-4"
         />
-        <h1 className="text-base font-medium">Documents</h1>
+        <h1 className="text-base font-medium">{getPageTitle(pathname)}</h1>
       </div>
     </header>
   );
