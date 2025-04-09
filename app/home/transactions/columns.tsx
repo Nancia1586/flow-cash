48592deea35434081522ab28bf94f5222c03cf6d
Nancia Rajerison <rajerisonnancia@gmail.com>
2025-04-09 19:12:41 +0300

    Integrate transactions datatable

--- app/home/transactions/columns.tsx
+++ app/home/transactions/columns.tsx
@@ -1,39 +1,82 @@
 "use client";
 
+import * as React from "react";
 import { ColumnDef } from "@tanstack/react-table";
+
+import { Badge } from "@/components/ui/badge";
+import { TransactionType } from "@/types/transaction";
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
 export type Transaction = {
-  id: string;
-  amount: number;
-  email: string;
-  date: string; // Format YYYY-MM-DD
+  id: number;
+  date: string;
+  description: string;
   category: string;
-  type: "DEPENSE" | "REVENU" | "EPARGNE";
+  type: TransactionType;
   debit: number;
   credit: number;
+};
+
+const typeBadgeVariant: {
+  [key: string]: { label: string; color: string };
+} = {
+  [TransactionType.INCOME]: { label: "Revenu", color: "bg-income-color" },
+  [TransactionType.EXPENSE]: { label: "Dépense", color: "bg-expense-color" },
+  [TransactionType.SAVING]: { label: "Epargne", color: "bg-saving-color" },
 };
 
 export const columns: ColumnDef<Transaction>[] = [
   {
     accessorKey: "date",
-    header: "Date",
+    header: () => <div className="pl-3">Date</div>,
+    cell: ({ row }) => <div className="pl-3 text-sm">{row.original.date}</div>,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "description",
+    header: "Description",
+    cell: ({ row }) => (
+      <div className="text-muted-foreground">{row.original.description}</div>
+    ),
   },
   {
     accessorKey: "category",
-    header: "Description",
+    header: "Catégorie",
+    cell: ({ row }) => (
+      <div className="w-32">
+        <Badge variant="outline" className="px-1.5 text-muted-foreground">
+          {row.original.category}
+        </Badge>
+      </div>
+    ),
+    enableHiding: false,
   },
   {
     accessorKey: "type",
-    header: "Type de transaction",
+    header: "Type",
+    cell: ({ row }) => (
+      <div className="w-32">
+        <Badge
+          className={`px-1.5 text-white ${
+            typeBadgeVariant[row.original.type].color
+          }`}
+        >
+          {typeBadgeVariant[row.original.type].label}
+        </Badge>
+      </div>
+    ),
   },
   {
     accessorKey: "debit",
     header: "Debit",
+    cell: ({ row }) => <div className="text-sm">{row.original.debit}</div>,
+    enableHiding: false,
   },
   {
     accessorKey: "credit",
     header: "Credit",
+    cell: ({ row }) => <div className="text-sm">{row.original.credit}</div>,
+    enableHiding: false,
   },
 ];
